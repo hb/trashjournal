c5f83e1ab21eeac619880d26bb91fd1dabcdb34f
Holger Berndt <berndth@gmx.de>
2009-07-09 00:06:43 +0200

    reorganisation

--- trashjournal.py
+++ trashjournal.py
@@ -4,23 +4,29 @@ pygtk.require('2.0')
 import gtk
 import gio
 import gobject
-import pynotify
 
 import datetime
+import subprocess
 
 class TrashJournal():
     """"A journal for the trash."""
-    def __init__(self):
-        pynotify.init("trashing monitor")
-        
-        fp = gio.File("trash://")
-        monitor = fp.monitor_directory()
-        monitor.connect("changed", self.file_changed)
 
+    # constants
+    DAYS_MODEL_COLUMNS = (str, int)
+    DAYS_MODEL_COLUMN_DISPLAY_STRING = 0
+    DAYS_MODEL_COLUMN_DAYS = 1
+    
+    FILES_MODEL_COLUMNS = (object, str, str, str)
+    FILES_MODEL_COLUMN_GFILE = 0
+    FILES_MODEL_COLUMN_FILENAME = 1
+    FILES_MODEL_COLUMN_DELETION_TIMESTAMP = 2
+    FILES_MODEL_COLUMN_FORMER_PATH = 3
+    
+    def __init__(self):
         # window
         self.main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.main_window.connect("destroy", gtk.main_quit)
-        self.main_window.set_size_request(500,300)
+        self.main_window.set_size_request(800,300)
         
         # vbox
         main_vbox = gtk.VBox(False, 2)
@@ -29,17 +35,18 @@ class TrashJournal():
         # horizonal pane
         pane = gtk.HPaned()
         main_vbox.pack_start(pane)
-
-        # list for days
+        
+        # days list
         scrolledwin = gtk.ScrolledWindow()
         scrolledwin.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         pane.add1(scrolledwin)
-        
-        model = gtk.ListStore(str, int)
+        model = gtk.ListStore(*TrashJournal.DAYS_MODEL_COLUMNS)
         treeview = gtk.TreeView(model)
         renderer = gtk.CellRendererText()
-        column = gtk.TreeViewColumn("Time", renderer, text=0)
+        # column "Time"
+        column = gtk.TreeViewColumn("Time", renderer, text=TrashJournal.DAYS_MODEL_COLUMN_DISPLAY_STRING)
         treeview.append_column(column)
+        # done with columns
         treeview.set_headers_visible(True)
         scrolledwin.add(treeview)
         selection_days = treeview.get_selection()
@@ -48,135 +55,110 @@ class TrashJournal():
         treeview.connect("button-press-event", self._days_view_button_pressed_cb);
         self._days_view = treeview
         self._days_model = model
-        
-        # list for files
+
+        # tree for files
         scrolledwin = gtk.ScrolledWindow()
         scrolledwin.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         pane.add2(scrolledwin)
-        
-        model = gtk.ListStore(str, str, object)
+        model = gtk.TreeStore(*TrashJournal.FILES_MODEL_COLUMNS)
         treeview = gtk.TreeView(model)
         treeview.connect("button-press-event", self._files_view_button_pressed_cb);
-
+        treeview.connect("row-activated", self._files_view_row_activated_cb);
+        treeview.connect("row-expanded", self._files_view_row_expanded_cb)
+        # column "Filename"
         renderer = gtk.CellRendererText()
-        column = gtk.TreeViewColumn("Filename", renderer, text=0)
+        column = gtk.TreeViewColumn("Filename", renderer, text=TrashJournal.FILES_MODEL_COLUMN_FILENAME)
         column.set_clickable(True)
         column.set_expand(True)
         column.set_resizable(True)
-        column.set_sort_column_id(0)
+        column.set_sort_column_id(TrashJournal.FILES_MODEL_COLUMN_FILENAME)
         treeview.append_column(column)
-        
+        # column "Deletion Timestamp"
         renderer = gtk.CellRendererText()
-        column = gtk.TreeViewColumn("Deletion Timestamp", renderer, text=1)
+        column = gtk.TreeViewColumn("Deletion Timestamp", renderer, text=TrashJournal.FILES_MODEL_COLUMN_DELETION_TIMESTAMP)
         column.set_clickable(True)
         column.set_expand(True)
         column.set_resizable(True)
-        column.set_sort_column_id(1)
+        column.set_sort_column_id(TrashJournal.FILES_MODEL_COLUMN_DELETION_TIMESTAMP)
         treeview.append_column(column)
-        
+        # column "Former Path"
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn("Former Path", renderer, text=TrashJournal.FILES_MODEL_COLUMN_FORMER_PATH)
+        column.set_clickable(True)
+        column.set_expand(True)
+        column.set_resizable(True)
+        column.set_sort_column_id(TrashJournal.FILES_MODEL_COLUMN_FORMER_PATH)
+        treeview.append_column(column)
+        # done with columns
         treeview.set_headers_visible(True)
         scrolledwin.add(treeview)
         selection = treeview.get_selection()
         selection.set_mode(gtk.SELECTION_MULTIPLE)
         self._files_view = treeview
         self._files_model = model
-        
-        # query trash
-        self._trash_content = self._sort_trash_content(self._get_trash_content())
+
+        # register changed handler for trash
+        fp = gio.File("trash://")
+        monitor = fp.monitor_directory()
+        monitor.connect("changed", self._trash_changed)
+
+        # update trash
         self._update_trash_content()
 
         # select first row in days view
-        selection_days.select_iter(self._days_model.get_iter_first())
+        iter = self._days_model.get_iter_first()
+        if iter:
+            selection_days.select_iter(iter)
 
         main_vbox.show_all()
         self.main_window.show()
         gtk.main()
 
-    def _fix_selection_after_button_press(self, view, event):
-            # Figure out which item they right clicked on
-            path = view.get_path_at_pos(int(event.x),int(event.y))
-            selection = view.get_selection()
-            # Get the selected path(s)
-            (model, paths) = selection.get_selected_rows()
-            # If the right click was not on a currently selected row, change the selection
-            if path[0] not in paths:
-                selection.unselect_all()
-                selection.select_path(path[0])        
+    def _get_child_gfile_from_fileinfo(self, file, fileinfo):
+        child = file.get_child(fileinfo.get_attribute_as_string("standard::name").decode('string_escape'))
+        if not child.query_exists():
+            child = file.get_child(fileinfo.get_attribute_as_string("standard::display-name"))
+            print 'needed display name: ' + fileinfo.get_attribute_as_string("standard::display-name")
+        if not child.query_exists():
+            print 'still not existing'
+            return None
+        return child
         
-    def _days_view_button_pressed_cb(self, view, event):
-        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
-            self._fix_selection_after_button_press(view, event)
-            self._days_view_popup_menu(event)
-            return True
-        
-    def _files_view_button_pressed_cb(self, view, event):
-        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
-            self._fix_selection_after_button_press(view, event)
-            self._files_view_popup_menu(event)
-            return True
-    def _days_view_popup_menu(self,event):
-        menu = gtk.Menu()
-        item = gtk.MenuItem(label="Do Something Else")
-        item.connect("activate", self._days_view_popup_do_something)
-        menu.append(item)
-        menu.show_all()
-        menu.popup(None, None, None, event.button, event.get_time())
-        
-    def _files_view_popup_menu(self, event):
-        menu = gtk.Menu()
-        item = gtk.MenuItem(label="Do Something")
-        item.connect("activate", self._files_view_popup_do_something)
-        menu.append(item)
-        menu.show_all()
-        menu.popup(None, None, None, event.button, event.get_time())
-    
-    def _get_fileinfo_list_from_file_view_selection(self, selection):
-        (model, pathlist) = selection.get_selected_rows()
-        fileinfo_list = []
-        for path in pathlist:
-            iter = model.get_iter(path)
-            fileinfo_list.append(model.get_value(iter, 2))
-        return fileinfo_list
-    
-    def _get_fileinfo_list_from_days_view_selection(self, selection):
-        (model, pathlist) = selection.get_selected_rows()
-        fileinfo_list = []
-        for path in pathlist:
-            iter = model.get_iter(path)
-            days = model.get_value(iter, 1)
-            fileinfo_list.extend(self._trash_content[days])
-        return fileinfo_list
-            
-    def _files_view_popup_do_something(self, item):
-        fileinfos = self._get_fileinfo_list_from_file_view_selection(self._files_view.get_selection())
-        print fileinfos
-        
-    def _days_view_popup_do_something(self, item):
-        fileinfos = self._get_fileinfo_list_from_days_view_selection(self._days_view.get_selection())
-        print fileinfos
-        
-    def _days_view_selection_changed_cb(self, selection):
-        """Days selection changed; build up file list to display"""
-        (model, pathlist) = selection.get_selected_rows()
-        for path in pathlist:
-            iter = model.get_iter(path)
-            days = model.get_value(iter, 1)
-            self._files_model.clear()
-            if days in self._trash_content:
-                for fileinfo in self._trash_content[days]:
-                    deletion_date = fileinfo.get_attribute_as_string("trash::deletion-date")
-                    if deletion_date:
-                        ts = self._get_datetime_from_deletion_date_string(deletion_date)
-                    else:
-                        ts = "<unknown>"
-                    self._files_model.append((fileinfo.get_attribute_as_string("standard::name").decode('string_escape'), ts, fileinfo))
-                
-    
     def _update_trash_content(self):
-        keys = self._trash_content.keys()
+        self._days_model.clear()
+        fp = gio.File("trash://")
+        enumerator = fp.enumerate_children("standard::name,standard::display-name,standard::type,trash::orig-path,trash::deletion-date")
+        # hash "days ago (int)" -> "gfile"
+        # special key values:
+        #  -1 : future
+        #  -2 : unknown (deletion date not set) 
+        self._days_hash = {}
+        today = datetime.date.today()
+        for fileinfo in enumerator:
+            child = self._get_child_gfile_from_fileinfo(fp, fileinfo)
+            if not child:
+                continue
+            deletion_date_string = fileinfo.get_attribute_string("trash::deletion-date")
+            if deletion_date_string:
+                deldate = self._get_datetime_from_deletion_date_string(deletion_date_string)
+                dt = today - deldate.date()
+                # sort faulty timestamps into a "future" value of -1 
+                if dt.days < 0:
+                    days = -1
+                else:
+                    days = dt.days                
+            else:
+                # unknown timestamps (deletion-date attribute not set)
+                days = -2
+            if not days in self._days_hash:
+                self._days_hash[days] = []
+            self._days_hash[days].append(child)
+        
+        # transform hash into model
+        keys = self._days_hash.keys()
         keys.sort()
         if not keys:
-            keys= []
+            keys = []
         for days in keys:
             if days == -2:
                 period_string = "Unknown"
@@ -188,47 +170,143 @@ class TrashJournal():
                 period_string = "Yesterday"
             else:
                 period_string = str(days) + " days ago"
-            iter = self._days_model.append([period_string, days])
-                
-        
-        
-    def _sort_trash_content(self, trash_content):
-        """Return a hash with integers as keys - 0 means today, 1 yesterday, 2 days ago etc."""
-        hash = {}
-        today = datetime.date.today()
-        for fileinfo in trash_content:
-            deletion_date_string = fileinfo.get_attribute_string("trash::deletion-date")
-            if deletion_date_string:
-                deldate = self._get_datetime_from_deletion_date_string(deletion_date_string)
-                dt = today - deldate.date()
-                # sort faulty timestamps into a "future" value of .1 
-                if dt.days < 0:
-                    days = -1
-                else:
-                    days = dt.days
-            else:
-                days = -2
+            
+            self._days_model.append([period_string, days])
 
-            if days not in hash:
-                hash[days] = []
-            hash[days].append(fileinfo)
-        return hash
-    
+
     def _get_datetime_from_deletion_date_string(self, string):
         return datetime.datetime.strptime(string, "%Y-%m-%dT%H:%M:%S")
+
+    def _add_file_to_files_model(self, file, fileinfo, parent_iter=None):
+        deletion_date = fileinfo.get_attribute_as_string("trash::deletion-date")
+        if deletion_date:
+            ts = self._get_datetime_from_deletion_date_string(deletion_date)
+        else:
+            ts = "<unknown>"
+        orig_path = fileinfo.get_attribute_as_string("trash::orig-path")
+        if not orig_path:
+            orig_path = "<unknown>"
+        iter = self._files_model.append(parent_iter, (file,
+                                                      fileinfo.get_attribute_as_string("standard::display-name").decode('string_escape'),
+                                                      ts,
+                                                      orig_path.decode('string_escape')))
         
+        # add a dummy child if this is a directory
+        if fileinfo.get_file_type() == gio.FILE_TYPE_DIRECTORY:
+            self._files_model.append(iter, (None, "Loading ...", "", ""))
+
+
+    def _add_directory_to_files_model(self, directory, parent_iter):
+        enumerator = directory.enumerate_children("standard::name,standard::display-name,standard::type,trash::orig-path,trash::deletion-date")
+        for fileinfo in enumerator:
+            child = self._get_child_gfile_from_fileinfo(directory, fileinfo)
+            self._add_file_to_files_model(child, fileinfo, parent_iter)
+
+
+    def _days_view_selection_changed_cb(self, selection):
+       """Days selection changed; build up file list to display"""
+       (model, pathlist) = selection.get_selected_rows()
+       for path in pathlist:
+            iter = model.get_iter(path)
+            days = model.get_value(iter, TrashJournal.DAYS_MODEL_COLUMN_DAYS)
+            self._files_model.clear()
+            if days in self._days_hash:
+                trash_fp = gio.File("trash://")
+                for file in self._days_hash[days]:
+                    fileinfo = file.query_info("standard::display-name,standard::name,standard::type,trash::orig-path,trash::deletion-date")
+                    self._add_file_to_files_model(file, fileinfo)
+ 
+        
+    def _trash_changed(self,filemonitor, file, other_file, event_type):
+        print 'TODO: trash changed'
     
-    def _get_trash_content(self):
-        """Returns a list of FileInfos of the trash folder"""
-        fp = gio.File("trash://")
-        enumerator = fp.enumerate_children("standard::name,trash::orig-path,trash::deletion-date")
-        return list(enumerator)
-            
-    def file_changed(self,filemonitor, file, other_file, event_type):
-        if event_type == gio.FILE_MONITOR_EVENT_CREATED:
-            notification = pynotify.Notification("Moved to Trash", file.get_basename(), icon=gtk.STOCK_DELETE)
-            notification.show()
-            
+    
+    def _fix_selection_after_button_press(self, view, event):
+        # Figure out which item they right clicked on
+        path = view.get_path_at_pos(int(event.x),int(event.y))
+        selection = view.get_selection()
+        # Get the selected path(s)
+        (model, paths) = selection.get_selected_rows()
+        # If the right click was not on a currently selected row, change the selection
+        if path[0] not in paths:
+            selection.unselect_all()
+            selection.select_path(path[0])        
+
+
+    def _days_view_button_pressed_cb(self, view, event):
+        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
+            self._fix_selection_after_button_press(view, event)
+            self._days_view_popup_menu(event)
+            return True
+
+
+    def _days_view_popup_menu(self,event):
+        menu = gtk.Menu()
+        item = gtk.MenuItem(label="Do Something Else")
+        item.connect("activate", self._days_view_popup_do_something)
+        menu.append(item)
+        menu.show_all()
+        menu.popup(None, None, None, event.button, event.get_time())
+
+       
+    def _days_view_popup_do_something(self, item):
+        files = self._get_file_list_from_days_view_selection(self._days_view.get_selection())
+        print 'days popup do something'
+        print files
+
+    
+    def _files_view_button_pressed_cb(self, view, event):
+        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
+            self._fix_selection_after_button_press(view, event)
+            self._files_view_popup_menu(event)
+            return True
+
+
+    def _files_view_popup_menu(self, event):
+        menu = gtk.Menu()
+        item = gtk.MenuItem(label="Do Something")
+        item.connect("activate", self._files_view_popup_do_something)
+        menu.append(item)
+        menu.show_all()
+        menu.popup(None, None, None, event.button, event.get_time())
+        
+
+    def _files_view_popup_do_something(self, item):
+        files = self._get_file_list_from_files_view_selection(self._files_view.get_selection())
+        print 'files popup do something'
+        print files
+
+    def _files_view_row_activated_cb(self, view, path, col):
+        print 'TODO: files row activated'
+
+    def _files_view_row_expanded_cb(self, view, parent_iter, path):
+        # check if first child has a GFile associated with it
+        iter = self._files_model.iter_nth_child(parent_iter, 0)
+        file = self._files_model.get_value(iter, TrashJournal.FILES_MODEL_COLUMN_GFILE)
+        if not file:
+            directory = self._files_model.get_value(parent_iter, TrashJournal.FILES_MODEL_COLUMN_GFILE)
+            self._add_directory_to_files_model(directory, parent_iter)
+            # remove dummy "Loading" entry
+            self._files_model.remove(iter)
+
+
+    def _get_file_list_from_files_view_selection(self, selection):
+        (model, pathlist) = selection.get_selected_rows()
+        files = []
+        for path in pathlist:
+            iter = model.get_iter(path)
+            files.append(model.get_value(iter, TrashJournal.FILES_MODEL_COLUMN_GFILE))
+        return files
+
+    def _get_file_list_from_days_view_selection(self, selection):
+        (model, pathlist) = selection.get_selected_rows()
+        files = []
+        for path in pathlist:
+            iter = model.get_iter(path)
+            days = model.get_value(iter, TrashJournal.DAYS_MODEL_COLUMN_DAYS)
+            files.extend(self._days_hash[days])
+        return files
+
 
 if __name__ == "__main__":
     mainwin = TrashJournal()
